82dc27ccb8fa7741b20ebe6d9af301f8e2aef96b
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-06-22 12:43:00 +0200

    Task-58476: Fix UT

--- agenda-connectors-services/src/test/java/org/exoplatform/agendaconnector/storage/ExchangeConnectorStorageTest.java
+++ agenda-connectors-services/src/test/java/org/exoplatform/agendaconnector/storage/ExchangeConnectorStorageTest.java
@@ -1,38 +1,57 @@
 package org.exoplatform.agendaconnector.storage;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
+import org.hibernate.ObjectNotFoundException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.exoplatform.agendaconnector.model.ExchangeUserSetting;
+import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.component.RequestLifeCycle;
 
-public class ExchangeConnectorStorageTest {
+import junit.framework.TestCase;
 
-    private  ExchangeConnectorStorage exchangeConnectorStorage;
+public class ExchangeConnectorStorageTest extends TestCase {
 
-    @Before
-    public void setUp() throws Exception { // NOSONAR
-        PortalContainer container = PortalContainer.getInstance();
-        exchangeConnectorStorage= container.getComponentInstanceOfType(ExchangeConnectorStorage.class);
-    }
+  private ExchangeConnectorStorage exchangeConnectorStorage;
 
-    @Test
-    public void testCreateExchangeSetting() {
+  private PortalContainer          container;
 
-        long userIdentityId = 1;
-        ExchangeUserSetting createdExchangeUserSetting = new ExchangeUserSetting();
-        createdExchangeUserSetting.setDomainName("exoplatform");
-        createdExchangeUserSetting.setUsername("Root");
-        createdExchangeUserSetting.setPassword("Root123");
-        exchangeConnectorStorage.createExchangeSetting(createdExchangeUserSetting, userIdentityId);
-        ExchangeUserSetting retrievedExchangeUserSetting = exchangeConnectorStorage.getExchangeSetting(userIdentityId);
-        assertNotNull(retrievedExchangeUserSetting);
-        assertEquals("exoplatform", retrievedExchangeUserSetting.getDomainName());
-        assertEquals("Root", retrievedExchangeUserSetting.getUsername());
-        assertEquals("Root123", retrievedExchangeUserSetting.getPassword());
+  @Before
+  public void setUp() throws Exception {
+    container = PortalContainer.getInstance();
+    exchangeConnectorStorage = container.getComponentInstanceOfType(ExchangeConnectorStorage.class);
+    begin();
+  }
 
-    }
+  @Test
+  public void testCreateExchangeSetting() {
+
+    long userIdentityId = 1;
+    ExchangeUserSetting createdExchangeUserSetting = new ExchangeUserSetting();
+    createdExchangeUserSetting.setDomainName("exoplatform");
+    createdExchangeUserSetting.setUsername("Root");
+    createdExchangeUserSetting.setPassword("Root123");
+    exchangeConnectorStorage.createExchangeSetting(createdExchangeUserSetting, userIdentityId);
+    ExchangeUserSetting retrievedExchangeUserSetting = exchangeConnectorStorage.getExchangeSetting(userIdentityId);
+    assertNotNull(retrievedExchangeUserSetting);
+    assertEquals("exoplatform", retrievedExchangeUserSetting.getDomainName());
+    assertEquals("Root", retrievedExchangeUserSetting.getUsername());
+    assertEquals("Root123", retrievedExchangeUserSetting.getPassword());
+  }
+
+  private void begin() {
+    ExoContainerContext.setCurrentContainer(container);
+    RequestLifeCycle.begin(container);
+  }
+
+  private void end() {
+    RequestLifeCycle.end();
+  }
+
+  @After
+  public void tearDown() throws ObjectNotFoundException {
+    end();
+  }
 }
