b48ae2e055188214088f8531fc3794f92b6c670d
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-06-23 16:20:19 +0200

    Remove useless IllegalAccessException

--- agenda-connectors-api/src/main/java/org/exoplatform/agendaconnector/service/ExchangeConnectorService.java
+++ agenda-connectors-api/src/main/java/org/exoplatform/agendaconnector/service/ExchangeConnectorService.java
@@ -40,7 +40,6 @@ public interface ExchangeConnectorService {
    * Deletes an exchange user setting
    *
    * @param userIdentityId User identity deleting his exchange user setting
-   * @throws IllegalAccessException when the user is not authorized to delete exchange user setting
    */
   void deleteExchangeSetting(long userIdentityId);
 }
